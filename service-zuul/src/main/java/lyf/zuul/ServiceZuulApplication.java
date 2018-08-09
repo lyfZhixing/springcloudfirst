04cb6042e3a57c1ca279110bbdaab0981dabd5a3
lyfZhixing <2214974097@qq.com>
2018-08-09 23:23:09 +0800

    新增：zuul模块启动类

--- service-zuul/src/main/java/lyf/zuul/ServiceZuulApplication.java
+++ service-zuul/src/main/java/lyf/zuul/ServiceZuulApplication.java
@@ -1,10 +1,25 @@
 package lyf.zuul;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
 /**
  * @Author: liyufeng
- * @Description:
+ * @Description: 注解@EnableZuulProxy，开启zuul的功能：
  * @Date: Created in 23:05 2018/8/9
  * @Modified By:
  */
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+@EnableDiscoveryClient
 public class ServiceZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceZuulApplication.class,args);
+    }
+
 }
