e87d3115f27a93928aa0d6e8b821d8430ceaf4d8
liyufeng <liyufenglyff@163.com>
2018-08-09 18:10:13 +0800

    改动：在FeignClient注解中加入fallback的指定类

--- serice-feign/src/main/java/lyf/feign/service/SchedualServiceHi.java
+++ serice-feign/src/main/java/lyf/feign/service/SchedualServiceHi.java
@@ -1,5 +1,6 @@
 package lyf.feign.service;
 
+import lyf.feign.serviceimpl.SchedualServiceHiHystric;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +11,9 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @Author: liyufeng
  * @Description:
  * @Date: Created in 下午4:59 18-8-9
- * @Modified By:
+ * @Modified By: liyufneg 下午5:59 18-8-9  添加断路器
  */
-@FeignClient(value = "servicehi")
+@FeignClient(value = "service-hi" , fallback = SchedualServiceHiHystric.class)
 public interface SchedualServiceHi {
 
     @RequestMapping(value = "/hi" , method = RequestMethod.GET)
