b8fe0e29d1ddd2de57719b82c1c783b0b3a54cbc
liyufeng <liyufenglyff@163.com>
2018-08-10 17:54:52 +0800

    新增：客户端

---
+++ spring-cloud-config/config-client/src/main/java/lyf/client/ConfigClientApplication.java
@@ -0,0 +1,29 @@
+package lyf.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: liyufeng
+ * @Description:
+ * @Date: Created in 下午3:56 18-8-10
+ * @Modified By:
+ */
+@SpringBootApplication
+@RestController
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+    @Value("${foo}")
+    String foo;
+    @GetMapping(value = "/hi")
+    public String hi(){
+        return foo;
+    }
+}
