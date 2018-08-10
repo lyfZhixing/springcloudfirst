f65c1ff7e18daeab3d002eac8a95c0d63c652490
liyufeng <liyufenglyff@163.com>
2018-08-10 17:55:23 +0800

    新增：服务端

---
+++ spring-cloud-config/config-server/src/main/java/lyf/config/ConfigServerApplication.java
@@ -0,0 +1,20 @@
+package lyf.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @Author: liyufeng
+ * @Description: EnableConfigServer注解开启配置服务器的功能
+ * @Date: Created in 下午3:20 18-8-10
+ * @Modified By:
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
