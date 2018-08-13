9c5d2ed7768f3583a398421d789d5b0fc01721b3
liyufeng <liyufenglyff@163.com>
2018-08-13 15:57:43 +0800

    修改：添加@RefreshScope

--- spring-cloud-config/config-client/src/main/java/lyf/client/ConfigClientApplication.java
+++ spring-cloud-config/config-client/src/main/java/lyf/client/ConfigClientApplication.java
@@ -3,6 +3,7 @@ package lyf.client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,11 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
  * @Author: liyufeng
  * @Description:
  * @Date: Created in 下午3:56 18-8-10
- * @Modified By:
+ * @Modified By: liyufeng 增加注解@RefreshScope
  */
 @SpringBootApplication
 @RestController
 @EnableEurekaClient
+@RefreshScope
 public class ConfigClientApplication {
 
     public static void main(String[] args) {
